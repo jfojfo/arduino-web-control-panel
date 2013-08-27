d04dcc90de9746faaafc073fcec8b5cc95d315ac
jfojfo <jfojfo@gmail.com>
2013-08-27 15:58:38 +0800

    programer works

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+$url_info = parse_url($_SERVER["REQUEST_URI"]);
+$path = $url_info['path'];
+$query = "programer";
+if (isset($url_info['query']))
+    $query = $url_info['query'];
+//parse_str($str, $query_arr);
+$tool = strtolower($query);
+$tool_page = "tool-" . $tool . ".php";
+if (!is_file(getcwd() . "/" . $tool_page)) {
+    $tool = "programer";
+    $tool_page = "tool-" . $tool . ".php";
+}
+
+$tool_map = array(
+    'programer' => 'Programer',
+    'serial' => 'Serial',
+    'camera' => 'Video Camera',
+    'cmd' => 'Command'
+);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Control Panel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+
+    <!-- Bootstrap -->
+<!--    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">-->
+<!--    <script src="js/jquery-1.7.1.min.js"></script>-->
+<!--    <script src="js/bootstrap-2.3.2.min.js"></script>-->
+    <script>
+        if (!window.jQuery) {
+            console.log("load script from baidu cdn...");
+            document.write('<link href="http://libs.baidu.com/bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet" media="screen">');
+            document.write('<script src="http://libs.baidu.com/jquery/1.10.2/jquery.js"><\/script>');
+            document.write('<script src="http://libs.baidu.com/bootstrap/2.3.2/js/bootstrap.min.js"><\/script>');
+        }
+    </script>
+    <link href="css/custom.css" rel="stylesheet" media="screen">
+</head>
+<body>
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="brand" href="">Control Panel</a>
+                <ul class="nav">
+                <?php foreach($tool_map as $key => $value): ?>
+                    <li<?php if($key === $tool) echo ' class="active"'?>>
+                        <a data-toggle="pill" href="<?php echo $path . '?' . $key; ?>"><?php echo $value; ?></a>
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+                <ul class="nav pull-right">
+                    <li>
+                        <a href="#" onclick="reboot()">Reboot</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <?php include($tool_page); ?>
+    </div>
+<script>
+    $('[data-toggle="pill"]').click(function(e){
+        e.preventDefault();
+        var url = $(this).attr("href");
+        window.location.href = url;
+    });
+</script>
+</body>
+</html>
---
+++ tool-programer.php
@@ -0,0 +1,44 @@
+<div class="tool-programer">
+    <span class="label label-warning">Choose file to upload:</span>
+    <div style="margin-top: 10px;">
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" name="upload" value="Go!">
+        </form>
+    </div>
+    <div>
+<?php
+if (isset($_POST['upload'])) {
+    $file = $_FILES["file"];
+    if ($file["error"] > 0) {
+        $upload_error_strings = array( false,
+            "The uploaded file exceeds the upload_max_filesize directive in php.ini.",
+            "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.",
+            "The uploaded file was only partially uploaded.",
+            "No file was uploaded.",
+            '',
+            "Missing a temporary folder.",
+            "Failed to write file to disk.",
+            "File upload stopped by extension."
+        );
+        echo "Error: " . $upload_error_strings[$file["error"]];
+    } else {
+        $name = $file["name"];
+        $size = $file["size"];
+        $output = shell_exec("avrdude -p m328p -c arduino -b 57600 -P /dev/ttyUSB0 -C ./avrdude.conf -U flash:w:".trim($name) . "2>&1");
+?>
+        <div class='alert'>
+            <span>Successfully uploaded <b><?php echo $name; ?></b> to the Arduino with avrdude </span>
+            <span>(size: <?php echo round($size/1024, 2); ?>Kb)</span>
+        </div>
+        <hr>
+        <span class="label label-warning">avrdude output:</span>
+        <div class="pre-container" style="margin-top: 10px; min-height: 100px;">
+          <pre class="clear-pre"><?php echo $output; ?></pre>
+        </div>
+<?php
+    }
+}
+?>
+    </div>
+</div>
